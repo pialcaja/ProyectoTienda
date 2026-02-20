8f9c499aae3b660cddf5822b3a5f026a29879635
pialcaja <pialcaja@gmail.com>
2026-02-20 16:11:15 -0500

    Construcción Back - Carrito, ItemCarrito

--- backend/src/main/java/com/ProyectoCarrito/producto/ProductoRepository.java
+++ backend/src/main/java/com/ProyectoCarrito/producto/ProductoRepository.java
@@ -1,9 +1,17 @@
 package com.ProyectoCarrito.producto;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductoRepository extends JpaRepository<Producto, Long> {
 
+    // List<Producto> findByEstado(Boolean estado);
+    
+    Optional<Producto> findByIdAndEstado(Long id, Boolean estado);
+    
+    // List<Producto> findByCategoriaIdAndEstado(Long categoriaId, Boolean estado);
 }
